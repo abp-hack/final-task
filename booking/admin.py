8e4c837027f1ca69049bbfad370dc656bf7c467d
Iliavas <il.vsl0110@gmail.com>
2023-03-15 13:55:56 +0300

    add all

--- booking/admin.py
+++ booking/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from .models import Booking
 from django.db.models import Q
 from django.core.exceptions import ValidationError
+from dynamic_admin_forms.admin import DynamicModelAdminMixin
+from application.models import HotelNumber
 
-# @admin.register(Booking)
-# class BookingAdmin(admin.ModelAdmin):
-#     class Meta:
-#         model = Booking
-#     fields = ('date_started', 'date_end', 'date_len', 'hotel')
+
+@admin.register(Booking)
+class BookingAdmin(DynamicModelAdminMixin, admin.ModelAdmin):
+    class Meta:
+        model = Booking
+    fields = ('date_started', 'date_end', 'date_len', 'hotel', 'number', 'cost')
+    dynamic_fields = ('number', )
+
+    def get_dynamic_number_field(self, data):
+        q = HotelNumber.objects.all()
+        if 'hotel' in data.keys():
+            q = q.filter(hotel=data['hotel'])
+        return HotelNumber.objects.filter(hotel=data['hotel']), data['number'], False
